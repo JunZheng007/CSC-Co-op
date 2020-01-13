7f481aa086cb3f0840bed191081707fc7ac7cd5f
JunZheng007 <i996zhjun@gmail.com>
2020-01-13 16:40:13 -0500

    sumbit answer for Quiz 1

---
+++ Quiz_1 question_2.sh
@@ -0,0 +1,19 @@
+for file in 'ls *.txt'
+do
+    mv $file new_$file
+done
+
+for file in 'ls *.bak'
+do
+    rm $file
+done
+
+for file in 'ls *.py'
+do
+    if [ wc -l $file -gt 10 ]
+    then 
+        mv $file long_$file
+    else
+        mv $file short_$file
+    fi
+done
